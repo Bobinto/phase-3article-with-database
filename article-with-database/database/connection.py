38bfaced92b6f0388b9c48b7eb940445807fb160
Bobinto <mansalati18@gmail.com>
2024-06-21 19:55:55 +0300

    update

---
+++ article-with-database/database/connection.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+DATABASE_NAME = './database/magazine.db'
+
+def get_db_connection():
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_db_connection():
+    conn = sqlite3.connect('magazine.db')
+    return conn
